21bead5fb5ceae3165a99e3f9f0ec39514b52f7a
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2022-06-16 12:10:12 -0700

    create server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const PORT = 5050;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/dropnote-db",
+  (err) => {
+    if (err) throw new Error({ msg: err });
+    console.log("Connected to dropnote-db!");
+  }
+);
+
+app.listen(PORT, () =>
+  console.log(`Now listening at http://localhost:${PORT}`)
+);
