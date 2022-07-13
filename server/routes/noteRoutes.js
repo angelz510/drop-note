8e2e829fe8d03b1e964024ae11f33e7de53c9aea
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2022-07-13 13:46:21 -0700

    delete comments and unused files before deployment

--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -5,8 +5,4 @@ const { getAllNotes, createNewNote } = require("../controllers/noteController");
 router.get("/", getAllNotes);
 router.post("/", createNewNote);
 
-// router.get("/", (req, res) => {
-//   res.send("success");
-// });
-
 module.exports = router;
